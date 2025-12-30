f33fe05848a4a94428773d6a784d20b47b304965
Ryan Rasti <ryanrasti@protonmail.com>
2025-12-29 22:45:01 -0800

    Add Deno as official sandbox

--- src/code-mode-runtime.ts
+++ src/code-mode-runtime.ts
@@ -8,19 +8,17 @@ import { StreamTransport } from './stream-transport'
 declare const __SANDBOX_CONTEXT_PROMISE__: Promise<{
   input: ReadableStream<Uint8Array>
   output: WritableStream<Uint8Array>
+  onSuccess: () => void
+  onFailure: () => void
 }> | undefined
 
-async function main() {
-  if (!__SANDBOX_CONTEXT_PROMISE__) {
-    throw new TypeError('__SANDBOX_CONTEXT_PROMISE__ was not replaced prior to execution.')
-  }
-
-  const { input, output } = await __SANDBOX_CONTEXT_PROMISE__
-
-  const transport = new StreamTransport(input, output)
+async function run(context: {
+  input: ReadableStream<Uint8Array>
+  output: WritableStream<Uint8Array>
+}) {
+  const transport = new StreamTransport(context.input, context.output)
   try {
     const session = new RpcSession<ToolApi>(transport)
-
     const api = session.getRemoteMain()
 
     // The actual runtime code to execute:
@@ -39,8 +37,21 @@ async function main() {
   }
 }
 
+async function main() {
+  if (!__SANDBOX_CONTEXT_PROMISE__) {
+    throw new TypeError('__SANDBOX_CONTEXT_PROMISE__ was not replaced prior to execution.')
+  }
+  const ctx = await __SANDBOX_CONTEXT_PROMISE__
+  const { input, output, onSuccess, onFailure } = ctx
+
+  return run({ input, output })
+    .then(() => {
+      onSuccess()
+    })
+    .catch((err) => {
+      console.error('runtime error', err)
+      onFailure()
+    })
+}
+
 main()
-  .catch((err) => {
-    console.error('runtime error', err)
-    return Promise.reject(err)
-  })
